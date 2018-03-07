4b2ff0e0a5fb68f963e70c995d0fa733850304b0
AngelaGlass <32469919+AngelaGlass@users.noreply.github.com>
2018-03-07 17:31:52 -0600

    Create lizandmepig.py

---
+++ lizandmepig.py
@@ -0,0 +1,19 @@
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+new_list= original.split(“ ”)
+pig_list = ""
+count = word
+word_count = len(string.split)
+
+while > = 1:
+  for i in new_list:
+    if len(original) > 0 and original.isalpha():
+      pig_
+      word = original.lower()
+      first = word [0]
+      new_word = word + first + pyg
+      new_word = new_word[1:len(new_word)]
+      .append('foo')
+    else:
+      print pig_list
